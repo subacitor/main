6e980f9bc07a141628e63fceb2202838e958996e
subacitor <91040105+subacitor@users.noreply.github.com>
2022-05-09 09:27:28 +0700

    Add files via upload

---
+++ main-main/main-main/WEB/web/myscripts.js
@@ -0,0 +1,57 @@
+// document.querySelectorAll(".carousel").forEach((carousel) => {
+//     const items = carousel.querySelectorAll(".carousel__item");
+//     const buttonsHtml = Array.from(items, () => {
+//       return `<span class="carousel__button"></span>`;
+//     });
+  
+//     carousel.insertAdjacentHTML(
+//       "beforeend",
+//       `
+//           <div class="carousel__nav">
+//               ${buttonsHtml.join("")}
+//           </div>
+//       `
+//     );
+  
+//     const buttons = carousel.querySelectorAll(".carousel__button");
+  
+//     buttons.forEach((button, i) => {
+//       button.addEventListener("click", () => {
+//         // un-select all the items
+//         items.forEach((item) =>
+//           item.classList.remove("carousel__item--selected")
+//         );
+//         buttons.forEach((button) =>
+//           button.classList.remove("carousel__button--selected")
+//         );
+  
+//         items[i].classList.add("carousel__item--selected");
+//         button.classList.add("carousel__button--selected");
+//       });
+//     });
+  
+//     // Select the first item on page load
+//     items[0].classList.add("carousel__item--selected");
+//     buttons[0].classList.add("carousel__button--selected");
+//   });
+
+
+  const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+  
